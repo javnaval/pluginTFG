48e34fc01d2baed36d4c9f351de3ee11f3363445
Jaime <jaical01@ucm.es>
2020-12-20 11:56:09 +0100

    aaaaa

--- data_structure/Plugin.h
+++ data_structure/Plugin.h
@@ -1,10 +1,12 @@
 #include <iostream>
+//#include <JuceHeader.h>
 #include "list"
 #include "ChannelPlugin.h"
 
 using namespace std;
 
-class Plugin {
+class Plugin	:	public juce::Component,
+					private juce::MidiKeyboardStateListener {
 	private:
 	//Rango que el teclado considera para introducir acordes
 	int range;
@@ -12,12 +14,53 @@ class Plugin {
     list<int> activeChannels;
     double startTime;
     double sampleRate = 44100.0;
+
+	juce::MidiKeyboardState keyboardState;
+    juce::MidiKeyboardComponent keyboardComponent;
     //
+
+	void handleNoteOn (juce::MidiKeyboardState*, int midiChannel, int midiNoteNumber, float velocity) override
+    {
+        if (! isAddingFromMidiInput)
+        {
+            auto m = juce::MidiMessage::noteOn (midiChannel, midiNoteNumber, velocity);
+            m.setTimeStamp (juce::Time::getMillisecondCounterHiRes() * 0.001);
+            // TRATAR EL MENSAJE m
+        }
+    }
+
+    void handleNoteOff (juce::MidiKeyboardState*, int midiChannel, int midiNoteNumber, float /*velocity*/) override
+    {
+        if (! isAddingFromMidiInput)
+        {
+            auto m = juce::MidiMessage::noteOff (midiChannel, midiNoteNumber);
+            m.setTimeStamp (juce::Time::getMillisecondCounterHiRes() * 0.001);
+            // TRATAR EL MENSAJE m
+        }
+    }
+
 	public:
-	Plugin();
-	void AddInstrument(int instrument, int channel);
+	Plugin()
+		: keyboardComponent (keyboardState, juce::MidiKeyboardComponent::horizontalKeyboard),
+    	  startTime (juce::Time::getMillisecondCounterHiRes() * 0.001)
+	{
+
+	
+		addAndMakeVisible (keyboardComponent);
+    	keyboardState.addListener (this);
+
+		setSize (600, 400);
+	}
+	
+	~Plugin()
+	{
+		keyboardState.removeListener (this);
+	}
+	void addInstrument(int instrument, int channel);
     void removeInstrument(int instrument);
 	void setTimer();
 	list<ChannelPlugin> getChannels();
 	void setRange(int range);
+
+	void processMIDIMessage(juce::MidiMessage m);
 };
